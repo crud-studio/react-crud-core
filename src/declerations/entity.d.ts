2adf7a9da5882f7f872f854734b1ac9249cc5fba
Aviv Mor <mo.aviv@gmail.com>
2021-05-27 12:37:08 +0300

    Entity and package urls changes.

--- src/declerations/entity.d.ts
+++ src/declerations/entity.d.ts
@@ -1,7 +1,11 @@
+import {FilterField, OrderDTO} from "./server";
+
 export interface BaseEntity {
     name: string;
     api: {
         path: string;
         cacheName: string;
+        defaultOrders: OrderDTO[];
+        customFilterFields?: FilterField[];
     };
 }
